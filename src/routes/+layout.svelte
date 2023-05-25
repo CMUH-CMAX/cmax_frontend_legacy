86ffaedddfcb6eae3251e788ef900998d2a64eb3
ZoneTwelve <zonetwelve159@gmail.com>
2023-05-26 00:18:16 +0800

    [update] Almost completed PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './Header.svelte';
-	import './styles.css';
+	import './styles.css';	
+
 </script>
 
 <svelte:head>
@@ -9,13 +10,36 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300&display=swap" rel="stylesheet">
 	<!-- Google fonts end -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CMAX 中醫地圖</title>
+	<link rel="manifest" href="/manifest.json">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="application-name" content="CMAX">
+	<meta name="apple-mobile-web-app-title" content="CMAX">
+	<meta name="msapplication-starturl" content="/">
+	<link rel="icon" type="image/png" sizes="128x128" href="CMAX_logo_darkblue-128.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="128x128" href="CMAX_logo_darkblue-128.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="CMAX_logo_darkblue-16.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="16x16" href="CMAX_logo_darkblue-16.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="CMAX_logo_darkblue-32.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="32x32" href="CMAX_logo_darkblue-32.png">
+	<link rel="icon" type="image/png" sizes="64x64" href="CMAX_logo_darkblue-64.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="64x64" href="CMAX_logo_darkblue-64.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="CMAX_logo_darkblue-96.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="96x96" href="CMAX_logo_darkblue-96.png">
+	<link rel="icon" type="image/png" sizes="256x256" href="CMAX_logo_darkblue-256.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="256x256" href="CMAX_logo_darkblue-256.png">
+	<link rel="icon" type="image/png" sizes="512x512" href="CMAX_logo_darkblue-512.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="512x512" href="CMAX_logo_darkblue-512.png">
+	<link rel="icon" type="image/svg" href="CMAX_logo_darkblue.svg">
+	<link rel="apple-touch-icon" type="image/svg" href="CMAX_logo_darkblue.svg">
 </svelte:head>
 
 <div class="app">
 	<Header />
 
-	<main>
+	<main>	
 		<slot />
 	</main>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,147 @@
 <script>
 		import { Bell, Squares2x2, FaceSmile, Map, ChatBubbleOvalLeftEllipsis, Eye } from 'svelte-heros-v2';
+		let lastname = '殊橋'
+		let symptomsRank = [
+			{
+				name: '發燒',
+				view: 3578,
+			},
+			{
+				name: '紅疹',
+				view: 1324
+			}
+		]
 </script>
 
-<div class="container">
-	<h1>Hello world</h1>
+<div class="">
+	<div class="top-content content-slot mx-auto grid grid-cols-4 gap-4 content-around p-5">
+		<div class="col-span-3 text-white">
+			<p class="text-xs">Hi，<span class="lastname font-semibold">{lastname}</span>！</p>
+			<p class="text-base">今天過得好嗎？</p>
+		</div>
+		<div class="flex justify-end">
+			<div class="notification">
+				<div class="notification-btn rounded-full w-10 h-10 flex justify-center items-center">
+					<Bell variation="outline" size="24" class="bell" />
+				</div>
+			</div>
+		</div>
+		<div class="col-span-4">
+			<p class="quicksearch-btn p-2 rounded-lg text-center">
+				尋找最適合的中醫及診所
+			</p>
+		</div>
+	</div>
+
+	<div class="signin-slot m-5 text-white flex justify-center items-center outline">
+		<h1>月兔</h1>
+	</div>
+
+	<div class="recent-slot grid m-5">
+
+		<div class="grid grid-cols-4 gap-4 pb-2 white-text">
+			<p class="col-span-3 text-sm gap-4 pb-2 white-text">最近常見症狀</p>
+			<p class="text-sm text-end">查看更多</p>
+		</div>
+
+		<div class="grid grid-cols-2">
+
+			<div class="symptoms-card flex col-span rounded px-5 py-3.5 text-sm">
+				<div class="flex justify-start">
+					{symptomsRank[0]['name']}
+				</div>
+				<div class="flex justify-end mr-0 ml-auto items-center">
+					<Eye variation="outline" size="14" class="eye" />
+					<span>{symptomsRank[0]['view']}</span>
+				</div>
+			</div>
+
+			<div class="symptoms-card flex col-span rounded px-5 py-3.5 text-sm">
+				<div class="flex justify-start">
+					{symptomsRank[1]['name']}
+				</div>
+				<div class="flex justify-end mr-0 ml-auto items-center">
+					<Eye variation="outline" size="14" class="eye" />
+					<span>{symptomsRank[1]['view']}</span>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="service-slot m-5">
+		<span class="text-white">服務</span>
+		<div class="service-card grid grid-cols-4 rounded-lg py-5">
+
+			<div class="service-items col">
+				<div class="service-icon flex justify-center items-center rounded-full mb-2">
+					<Squares2x2 color="white" size="24"/>
+				</div>
+				<span class="text-sm">症狀分類</span>
+			</div>
+
+			<div class="service-items col">
+				<div class="service-icon flex justify-center items-center rounded-full mb-2">
+					<FaceSmile color="white" size="24"/>
+				</div>
+				<span class="text-sm">症狀檢查器</span>
+			</div>
+
+			<div class="service-items col">
+				<div class="service-icon flex justify-center items-center rounded-full mb-2">
+					<Map color="white" size="24"/>
+				</div>
+				<span class="text-sm">診所地圖</span>
+			</div>
+
+			<div class="service-items col">
+				<div class="service-icon flex justify-center items-center rounded-full mb-2">
+					<ChatBubbleOvalLeftEllipsis color="white" size="24"/>
+				</div>
+				<span class="text-sm">許願池</span>
+			</div>
+
+		</div>
+	</div>
 </div>
+
+<style>
+	.notification-btn {
+		color: var(--blue-3);
+		background: var(--white-f);
+	}
+	.quicksearch-btn {
+		color: var(--gray-5);
+		background: var(--white-f);
+	}
+
+	.signin-slot {
+		height: 250px;
+	}
+
+	.symptoms-card {
+		background: var(--white-75p);
+	}
+	.symptoms-card:first-child{
+		margin-right: 5px;
+	}
+	.symptoms-card:last-child{
+		margin-left: 5px;
+	}
+
+	.service-card {
+		background: var(--white-75p);
+	}
+
+	.service-items {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.service-icon {
+		width: 57px;
+		height: 57px;
+		background: var(--linear-2);
+	}
+</style>
